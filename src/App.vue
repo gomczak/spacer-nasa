ce547d425b19d02d26787815689a2930aa299ad0
gomczak <tomczakmalgorzata2@gmail.com>
2020-07-22 19:08:53 +0200

    added claim component and simple fixes

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,13 @@
 </template>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap');
 
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
 </style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -41,7 +41,7 @@ export default {
         .catch((error) => {
           console.log(error);
         });
-    }, 500),
+    }, 400),
   },
 };
 </script>

75bcc8ffaecec94604604cc98f7a2855d49a3720
gomczak <tomczakmalgorzata2@gmail.com>
2020-07-22 21:30:07 +0200

    added search input and deleted router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="app">
-    <div class="nav">
-      <router-link to="/">spacer</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="wrapper">
+    <HeroImage />
+    <Claim />
+    <SearchInput v-model="searchValue" @input="handleInput"/>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import Claim from '@/components/Claim.vue';
+import SearchInput from '@/components/SearchInput.vue';
+import HeroImage from '@/components/HeroImage.vue';
+
+const API = 'https://images-api.nasa.gov/search';
+
+export default {
+  name: 'App',
+  components: {
+    HeroImage,
+    Claim,
+    SearchInput,
+  },
+  data() {
+    return {
+      searchValue: '',
+      results: [],
+    };
+  },
+  methods: {
+    // eslint-disable-next-line
+    handleInput: debounce(function () {
+      console.log(this.searchValue);
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          this.results = response.data.collection.items;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 400),
+  },
+};
+</script>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;800&display=swap');
@@ -19,5 +55,17 @@
     font-family: 'Montserrat', sans-serif;
     margin: 0;
     padding: 0;
+  }
+
+  .wrapper {
+    margin: 0;
+    width: 100%;
+    min-height: 100vh;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
   }
 </style>
---
+++ src/components/HeroImage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="heroImage" />
+</template>
+<script>
+export default {
+  name: 'HeroImage',
+};
+</script>
+<style lang="scss" scoped>
+    .heroImage {
+        position: fixed;
+        top: 0;
+        z-index: -1;
+        width: 100%;
+        height:100vh;
+        background-image: url('../assets/heroimage.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: 40% 0%;
+    }
+
+    .heroImage::after {
+        content: '';
+        background: rgba(255, 255, 255, 0.05);
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        width: 100%;
+        height: 100vh;
+    }
+</style>
